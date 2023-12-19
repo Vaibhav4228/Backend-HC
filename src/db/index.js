1e854e956963e4fd76a88d153bcfc03ad08ffbc8
Vaibhav422 <92776886+Vaibhav992@users.noreply.github.com>
2023-12-19 22:53:28 +0530

    added feature

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONOGODB_URI}${DB_NAME}`)
-        console.log(process.env.MONGODB_URI )
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(process.env.MONGODB_URI)
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-        // console.log(process.env.MONGODB_URI )
+        // console.log(process.env.MONGODB_URI)
     } catch (error) {
         console.log("MONGODB connection FAILED ", error);
         process.exit(1)
     }
 }
 
-export default connectDB
+export default connectDB;
